be42b7716bbe13497f5ba0d301d8384d7e341aaf
BigJimThunder <128534319+BigJimThunder@users.noreply.github.com>
2025-02-12 01:06:07 +0000

    rrrewqr

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,12 @@ class DirectorsController < ApplicationController
   def main
     render({ :template => "misc_templates/directors_main"})
   end
+
+  def youngest
+    render({ :template => "misc_templates/directors_youngest"})
+  end
+
+  def eldest
+    render({ :template => "misc_templates/directors_eldest"})
+  end 
 end
